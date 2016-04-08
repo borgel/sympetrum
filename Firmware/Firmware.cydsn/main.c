72ebe29954320922cfc619fed620ad2f15edfd4b
borgel <iborgel@gmail.com>
2016-04-07 22:45:45 -0700

    More dotstar experiments
    Still not much luck

--- Firmware/Firmware.cydsn/main.c
+++ Firmware/Firmware.cydsn/main.c
@@ -41,29 +41,58 @@ int main()
     SPI_LED_Start();
 
     
-    struct led_data wat = {0x7, 0x8, {0, 0, 10}};
+    struct led_data wat = {0x7, 0x1F, {0, 0, 10}};
     
     uint8_t count = 0;
     int len, j, i;
     uint8_t buf[100];
+    
+    //start frame
+    buf[0] = buf[1] = buf[2] = buf[3] = 0x00;
+    
+    //an LED
+    buf[4] = 0xFF;
+    buf[5] = 0x00;
+    buf[6] = 0xAA;
+    buf[7] = 0x00;
+    
+    
+    buf[ 8] = 0xFF;
+    buf[ 9] = 0x00;
+    buf[10] = 0xAA;
+    buf[11] = 0x00;
+    
+    //end frame
+    buf[12] = buf[13] = buf[14] = buf[15] = 0xFF;
+    
+    SPI_LED_PutArray(buf, 16);
+    
     for(;;) {
         
+        /*
         //start frame
         SPI_LED_WriteByte(0x00);
         SPI_LED_WriteByte(0x00);
         SPI_LED_WriteByte(0x00);
         SPI_LED_WriteByte(0x00);
         
-        for(i = 0; i < 6; i++) {
+        for(i = 0; i < 1; i++) {
             //1 LED
-            SPI_LED_PutArray((uint8_t*)&wat, sizeof(wat));
+            //SPI_LED_PutArray((uint8_t*)&wat, sizeof(wat));
+            
+            SPI_LED_WriteByte(0xFF);    //3 1's + full global brightness
+            SPI_LED_WriteByte(0x00);    //b
+            SPI_LED_WriteByte(0x00);    //g
+            SPI_LED_WriteByte(0xFF);    //r
         }
         
-        //end frame
+        
         SPI_LED_WriteByte(0xFF);
         SPI_LED_WriteByte(0xFF);
         SPI_LED_WriteByte(0xFF);
         SPI_LED_WriteByte(0xFF);
+        */
+        
         
         CyDelay(500);
         count++;
