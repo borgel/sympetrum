df5459cff17f6958101d919dfb3d4f3b5375455a
borgel <iborgel@gmail.com>
2016-06-05 11:00:01 -0700

    PSOC: Improve IR RX code (mostly untested)
    Also don't validate IR messages for now

--- Firmware/Firmware.cydsn/ir.c
+++ Firmware/Firmware.cydsn/ir.c
@@ -1,4 +1,6 @@
 #include "ir.h"
+#include "leds.h"
+#include "debprint.h"
 
 #include <project.h>
 #include <stdbool.h>
@@ -16,22 +18,36 @@ union ir_RawMessage {
     }__attribute__((packed));
 }__attribute__((packed));
 
+
+static bool StartDataRX;
+CY_ISR(IncomingIRISR) {
+    //debprint("Incoming ISR? Starting SPI RX\n");
+    
+    StartDataRX = true;
+}
+
+
 static bool ir_ValidateMessage(union ir_RawMessage *msg);
 
 void ir_Start(void) {
     IR_Transceiver_Start();
     PWM_1_Start();
     IRLedControl_Write(0);
+    StartDataRX = false;
+    IRDataIncomingInter_StartEx(IncomingIRISR);
 }
 
 void ir_Send(struct ir_Message *msg) {
     union ir_RawMessage m = {.msg = *msg, .toggle = 1};
     
-    //TODO explicitely enable/disable LED outputs around this
     IRLedControl_Write(1);
     IR_Transceiver_WriteTxData(m.raw);
     //IR_Transceiver_WriteTxData(0xF0F);
     IRLedControl_Write(0);
+}
+
+static void ir_PrintMessage(struct ir_Message *const m) {
+    debprint("Addr: 0x%x - Data 0x%x\n", m->address, m->command);
 }
 
 bool ir_Receive(struct ir_Message *msg) {
@@ -39,11 +55,17 @@ bool ir_Receive(struct ir_Message *msg) {
     
     m.raw = IR_Transceiver_ReadRxData();
     
+    *msg = m.msg;
+    return true;
+    
+    //FIXME re-enable
+    /*
     if(ir_ValidateMessage(&m)) {
         *msg = m.msg;
         return true;
     }
     return false;
+    */
 }
 
 static bool ir_ValidateMessage(union ir_RawMessage *msg) {
@@ -57,4 +79,22 @@ static bool ir_ValidateMessage(union ir_RawMessage *msg) {
         return false;
     }
     return true;
+}
+
+void ir_GiveTime(void) {
+    if(StartDataRX) {
+        debprint("Starting rx...\n");
+        
+        struct ir_Message m;
+        ir_Receive(&m);
+        
+        debprint("Message received! Contents are\n");
+        ir_PrintMessage(&m);
+        
+        //TODO react
+        
+        led_DisplayPattern();
+        
+        StartDataRX = false;
+    }
 }
