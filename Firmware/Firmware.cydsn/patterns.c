a343a1469f9c1eb0ac0cabb8f7dd9808ebe3ddf5
borgel <iborgel@gmail.com>
2016-07-03 23:21:27 -0700

    PSOC: Crappy first pass at LED fading

--- Firmware/Firmware.cydsn/patterns.c
+++ Firmware/Firmware.cydsn/patterns.c
@@ -5,7 +5,23 @@
 
 #include <project.h>
 
+#define MAX_TENDENCY                    2
+#define ITERATIONS_UNTIL_CHANGE         10  //30Hz framerate
+#define ITERATIONS_UNTIL_TENDENCIES     10
+
+/*
+color: The current color to display at this LED.
+tendency: The rate at which 
+*/
+struct pattern_AnimationState {
+    struct led_PackedColor color;
+    int8_t tendency;
+};
+struct pattern_AnimationState animation[LED_CHAIN_LENGTH] = {};
+
 struct pattern_State {
+    int untilNextChange;
+    int untilNextTendencies;
 };
 static const struct pattern_State stateNull = {};
 static struct pattern_State state;
@@ -35,16 +51,49 @@ void patterns_Start(void) {
 Be clever
 */
 void pattern_PermutePattern(void) {
-    //set some LEDs
-    struct led_PackedColor c = {0, 0, 0};
+    unsigned int i;
     
-    int i;
-    for(i = 0; i < LED_CHAIN_LENGTH; i++) {
-        c.b = rng_GetByte()/9;
-        c.r = rng_GetByte()/9;
-        c.g = rng_GetByte()/9;
-        led_SetColor(i, &c);
+    //periodically change the fade directions of the LEDs
+    if(state.untilNextChange == 0) {
+        state.untilNextChange = ITERATIONS_UNTIL_CHANGE;
+        
+        //TODO do pattern stuff
+        for(i = 0; i < sizeof(animation); i++) {
+            //if(state.untilNextTendencies == 0)
+            {
+                state.untilNextTendencies = ITERATIONS_UNTIL_TENDENCIES;
+                
+                if(rng_IsCoinHeads()) {
+                    animation[i].tendency++;
+                }
+                else {
+                    animation[i].tendency--;
+                }
+                
+                //clamp the tendency
+                if(animation[i].tendency > MAX_TENDENCY) {
+                    animation[i].tendency = MAX_TENDENCY;
+                }
+                else if(animation[i].tendency < -MAX_TENDENCY) {
+                    animation[i].tendency = -MAX_TENDENCY;
+                }
+                
+                //TODO if a channel is railed, reverse its tendency
+            }
+            
+            //TODO check if < min or > max and clamp
+            
+            //apply the tendency to all color channels
+            animation[i].color.r += animation[i].tendency;
+            animation[i].color.g += animation[i].tendency;
+            animation[i].color.b += animation[i].tendency;
+        }
     }
     
+    for(i = 0; i < sizeof(animation); i++) {
+        led_SetColor(i, &animation[i].color);
+    }
     
+    state.untilNextChange--;
+    state.untilNextTendencies--;
 }
