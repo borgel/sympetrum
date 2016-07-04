3d5231bd16d4c9c3e5e55dba9949b1c986aa3857
borgel <iborgel@gmail.com>
2016-07-04 00:54:28 -0700

    PSOC: Generate less 'balanced' random colors

--- Firmware/Firmware.cydsn/color.c
+++ Firmware/Firmware.cydsn/color.c
@@ -6,8 +6,27 @@
 
 #include <project.h>
 
+/*
+Get a random color weighted towards an extreme in one channel.
+*/
 void color_GetRandomColor(struct color_PackedColor* c) {
-    c->r = rng_GetByte();
-    c->g = rng_GetByte();
-    c->b = rng_GetByte();
+    switch(rng_GetByte() % 3) {
+    case 0:
+        c->r = rng_GetByte();
+        c->g /= 3;
+        c->b /= 3;
+        break;
+        
+    case 1:
+        c->r /= 3;
+        c->g = rng_GetByte();
+        c->b /= 3;
+        break;
+    
+    case 2:
+        c->r /= 3;
+        c->g /= 3;
+        c->b = rng_GetByte();
+        break;
+    }
 }
