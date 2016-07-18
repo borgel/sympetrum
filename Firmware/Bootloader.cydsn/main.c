9523b5a5b9d0ff9653244a1c023294d7795a77d9
borgel <iborgel@gmail.com>
2016-07-17 22:27:48 -0700

    PSOC: Fill in bootloader functionality
    Waits for 1s to receive an image over the UART, otherwise starts user
    code.

--- Firmware/Bootloader.cydsn/main.c
+++ Firmware/Bootloader.cydsn/main.c
@@ -1,26 +1,32 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
 #include <project.h>
 
 int main()
 {
-    CyGlobalIntEnable; /* Enable global interrupts. */
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    CyGlobalIntEnable;
+    
+    // turn off user LED
+    UserLED_Write(0);
+    
+    /*
+    //TODO if button pressed, do bootloader flow
+    //TODO make it wait forever (or long time)
+    if(UserButton_Read() == 0) {
+        //start the bootloader flow
+        UserLED_Write(1);
+        Bootloader_1_Start();
+    }
+    else {
+        UserLED_Write(0);
+        //jump directly into application
+        Bootloader_1_Exit(Bootloader_1_EXIT_TO_BTLDB);
+    }
+    */
+    
+    //start the bootloader flow
+    Bootloader_1_Start();
+    
     for(;;)
     {
-        /* Place your application code here. */
     }
 }
 
-/* [] END OF FILE */
