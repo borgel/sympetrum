91125815e92f96dfe136d776038a8662d1a45b84
borgel <iborgel@gmail.com>
2016-07-27 21:08:43 -0700

    PSOC: Purge dead systime code and rewrite it
    Works now. Also only triggers an interrupt every ~17minutes on 1kHz
    timer/counter rollover.

--- Firmware/Firmware.cydsn/systime.c
+++ Firmware/Firmware.cydsn/systime.c
@@ -2,40 +2,28 @@
 
 #include <project.h>
 
-//#define USE_WDT_SYSCLK
-
+#define TIMER_ROLLOVER  65535
 static systime_t time;
 
-//TODO only tick on rollover, and check timer's value every get time
-CY_ISR(SystimeTickCB) {
-#ifdef USE_WDT_SYSCLK
-    //cleared in sys interrupt handler?
-    CySysWdtClearInterrupt(CY_SYS_WDT_COUNTER0_INT);
-#else
+CY_ISR(SystimeTimerRollover) {
     SystimeTimer_ClearInterrupt(SystimeTimer_INTR_MASK_TC);
     SystimeTickInterrupt_ClearPending();
-#endif//USE_WDT_SYSCLK
 
-    time++;    
+    time += TIMER_ROLLOVER;    
 }
 
 void systime_Start(void) {
-#ifdef USE_WDT_SYSCLK
-    //configure watchdog to act as a time
-    CySysWdtSetMode(CY_SYS_WDT_COUNTER0, CY_SYS_WDT_MODE_INT);
-    CySysWdtSetMatch(CY_SYS_WDT_COUNTER0, 32);
-    CySysWdtSetClearOnMatch(CY_SYS_WDT_COUNTER0, 1);
-    CySysWdtSetInterruptCallback(CY_SYS_WDT_COUNTER0, systimeCB);
-    CySysWdtEnable(CY_SYS_WDT_COUNTER0_MASK);
-    CySysWdtDisable(CY_SYS_WDT_COUNTER1_MASK | CY_SYS_WDT_COUNTER1_MASK);
-    while(!CySysWdtGetEnabledStatus(CY_SYS_WDT_COUNTER0)) {}
-#else
+    time = 0;
+    
     SystimeClock_Start();
-    SystimeTickInterrupt_StartEx(SystimeTickCB);
+    SystimeTickInterrupt_StartEx(SystimeTimerRollover);
     SystimeTimer_Start();
-#endif//USE_WDT_SYSCLK
 }
 
 systime_t systime_GetTimeMS(void){
-    return time;
+    uint8_t flags = CyEnterCriticalSection();
+    systime_t t = time + SystimeTimer_ReadCounter();
+    CyExitCriticalSection(flags);
+    
+    return t;
 }
